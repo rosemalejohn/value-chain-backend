e13b02d370869b81db864ba2ebded13e472cc8c6
John Villacorta <rosemalejohn@gmail.com>
2022-10-19 08:46:48 +0800

    Add mailgun mailer

--- resources/views/emails/task_step_updated.blade.php
+++ resources/views/emails/task_step_updated.blade.php
@@ -3,5 +3,11 @@
 
 The task has been moved to {{ $task->step->description() }}.
 
+Task: {{ $task->title }} <br />
+Description: {!! $task->description !!} <br />
+Outcome: {!! $task->outcome !!} <br />
+Priority: {{ optional($task->priority)->description() }} <br />
+Impact: {{ optional($task->impact)->description() }} <br />
+
 The {{ config('app.name') }} Team. <br>
 @endcomponent
